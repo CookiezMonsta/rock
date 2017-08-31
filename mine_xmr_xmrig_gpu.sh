569daf4b1d89f9c57181f61529dc3bc02ea98c64
Boris Feinstein <madhat1@gmail.com>
2017-08-31 23:41:40 +0300

    Added xmrig-nvidia and xmrMiner binaries

---
+++ mine_xmr_xmrig_gpu.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Set HugePages to 12GB
+# sudo sysctl -w vm.nr_hugepages=6144
+
+# Bitfinex
+#/home/minion/rock/xmrig_v2.3.1/xmrig -o xmr-eu.dwarfpool.com:8005 -u 4BCeEPhodgPMbPWFN1dPwhWXdRX8q4mhhdZdA1dtSMLTLCEYvAj9QXjXAfF7CugEbmfBhgkqHbdgK9b2wKA6nqRZQCgvCDm.4ab3257714e5fe1097d17e25e0619aaaea051224a11b4f0a51c27e1b963735a1.loom -p x -k
+
+# mymonero.com
+sudo /home/minion/rock/xmrig-nvidia_v2.3.1/xmrig-nvidia --url=xmr-eu.dwarfpool.com:8050 --user=45nk55hPwc4Ghbie9QjbDdBXfpWz5V1s5Voo172hMWmPGiH7kLB29oVd7FqrutXQAye8D1PAHqiMidkLouTVGsDxEaak8hx.loom_gpu --pass=x --keepalive --donate-level=1
+
