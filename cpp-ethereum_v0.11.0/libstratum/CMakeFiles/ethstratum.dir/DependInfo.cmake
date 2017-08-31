31345a5f373fea81342f803cf4e0029987b26962
Boris Fenstein <madhat1@gmail.com>
2017-08-31 21:02:06 +0300

    New repos

---
+++ cpp-ethereum_v0.11.0/libstratum/CMakeFiles/ethstratum.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/cpp-ethereum/libstratum/EthStratumClient.cpp" "/home/ubuntu/cpp-ethereum/build/libstratum/CMakeFiles/ethstratum.dir/EthStratumClient.cpp.o"
+  "/home/ubuntu/cpp-ethereum/libstratum/EthStratumClientV2.cpp" "/home/ubuntu/cpp-ethereum/build/libstratum/CMakeFiles/ethstratum.dir/EthStratumClientV2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ETH_ETHASHCL"
+  "ETH_STRATUM"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../libstratum/.."
+  "/home/ubuntu/.hunter/_Base/435b09a/cd01ff3/0452b62/Install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
