b6195f8bce1849828e5324c44033c1768c402bf3
Boris Feinstein <borisf@sirinlabs.com>
2017-10-03 16:49:34 +0300

    sc_mine_cpu.sh

--- xmr/mine_xmr_xmrig.sh
+++ xmr/mine_xmr_xmrig.sh
@@ -10,5 +10,5 @@ HOSTNAME=$(hostname)
 THREADS=$CPUS
 
 # mymonero.com
-sudo ./xmrig_v2.3.1/xmrig --url=xmr-eu.dwarfpool.com:8005 --user=45nk55hPwc4Ghbie9QjbDdBXfpWz5V1s5Voo172hMWmPGiH7kLB29oVd7FqrutXQAye8D1PAHqiMidkLouTVGsDxEaak8hx.$HOSTNAME --pass=x --keepalive --donate-level=1 --threads=$CPUS --cpu-priority=5
+sudo /home/borisf/rock/xmr/xmrig_v2.3.1/xmrig --url=xmr-eu.dwarfpool.com:8005 --user=45nk55hPwc4Ghbie9QjbDdBXfpWz5V1s5Voo172hMWmPGiH7kLB29oVd7FqrutXQAye8D1PAHqiMidkLouTVGsDxEaak8hx.$HOSTNAME --pass=x --keepalive --donate-level=1 --threads=$CPUS --cpu-priority=5
 
