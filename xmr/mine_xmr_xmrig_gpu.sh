ddfdb452bfc0ac251ad72fca21883b2586776f26
Boris Feinstein <madhat1@gmail.com>
2018-04-29 17:05:15 +0300

    Added xmrig-nvidia v2.6.1

--- xmr/mine_xmr_xmrig_gpu.sh
+++ xmr/mine_xmr_xmrig_gpu.sh
@@ -3,6 +3,7 @@
 ROCK_DIR=$(dirname "${BASH_SOURCE[0]}")
 HOSTNAME=$(hostname)
 HOSTNAME=${HOSTNAME:0:8}-GPU
+XMRIG_VER=v2.6.1
 
 # mymonero.com
-sudo $ROCK_DIR/xmrig-nvidia_v2.3.1/xmrig-nvidia --url=xmr-eu.dwarfpool.com:8050 --user=45nk55hPwc4Ghbie9QjbDdBXfpWz5V1s5Voo172hMWmPGiH7kLB29oVd7FqrutXQAye8D1PAHqiMidkLouTVGsDxEaak8hx.$HOSTNAME --pass=x --keepalive --donate-level=1
+sudo $ROCK_DIR/xmrig-nvidia_$XMRIG_VER/xmrig-nvidia --url=xmr-eu.dwarfpool.com:8050 --user=45nk55hPwc4Ghbie9QjbDdBXfpWz5V1s5Voo172hMWmPGiH7kLB29oVd7FqrutXQAye8D1PAHqiMidkLouTVGsDxEaak8hx.$HOSTNAME --pass=x --keepalive --donate-level=1
